160370c5bdbde610cf5da6e752eaa2f173deccb9
crawlingweapon <crawling.weapon700@ok9.io>
2026-07-02 22:27:23 -0500

    fix: sanitize

--- custom_components/vikunja/const.py
+++ custom_components/vikunja/const.py
@@ -4,7 +4,6 @@ DEFAULT_URL = ""
 DEFAULT_SCAN_INTERVAL = 300
 
 CONF_URL = "url"
-CONF_API_TOKEN = "api_token"
 CONF_FILTERS = "filters"
 CONF_FILTER_ID = "filter_id"
 CONF_FILTER_NAME = "filter_name"
