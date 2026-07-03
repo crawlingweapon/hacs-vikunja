0d3d18a5fd5375c02ac4f553f0da4b766eafd598
crawlingweapon <crawling.weapon700@ok9.io>
2026-07-02 22:32:55 -0500

    fix: align const.py with imports

--- custom_components/vikunja/const.py
+++ custom_components/vikunja/const.py
@@ -4,7 +4,7 @@ DEFAULT_URL = ""
 DEFAULT_SCAN_INTERVAL = 300
 
 CONF_URL = "url"
-CONF_AUTH_KEY = "auth_token"
+CONF_VK_AT = "auth_token"
 CONF_FILTERS = "filters"
 CONF_FILTER_ID = "filter_id"
 CONF_FILTER_NAME = "filter_name"
