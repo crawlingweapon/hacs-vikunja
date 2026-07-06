978e67bd8c60fda35efd580f89a866f195612894
crawlingweapon <crawling.weapon700@ok9.io>
2026-07-06 09:39:16 -0500

    fix: validate token, resolve sensor crash

--- custom_components/vikunja/sensor.py
+++ custom_components/vikunja/sensor.py
@@ -31,6 +31,7 @@ async def async_setup_entry(
 ) -> None:
     """Set up Vikunja sensors based on a config entry."""
     url = entry.data[CONF_URL].rstrip("/")
+    api_token = entry.data[CONF_VK_AT]
     filters = entry.data.get(CONF_FILTERS, [])
 
     coordinator = VikunjaDataCoordinator(hass, url, api_token, filters)
